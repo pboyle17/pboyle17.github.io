880534975fb52532f31e56e54e73615b21d9858d
Paul Boyle <pboyle17@gmail.com>
2015-11-10 21:38:21 -0600

    night update

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -1,15 +1,11 @@
 $(document).ready(function(){
 
-  player1.makeAMove(prompt('What column do you want to place a piece in?'));
-  player2.makeAMove(prompt('What column do you want to place a piece in?'));
-  player1.makeAMove(prompt('What column do you want to place a piece in?'));
-  player2.makeAMove(prompt('What column do you want to place a piece in?'));
-  player1.makeAMove(prompt('What column do you want to place a piece in?'));
-  player2.makeAMove(prompt('What column do you want to place a piece in?'));
 
-  gameBoard.printBoard();
 
-});
+  gameBoard.playConnectFour();
+
+
+});//end of document.ready()
 
 var gameBoard = {
 
@@ -22,15 +18,85 @@ var gameBoard = {
   checkForWinner:function(){
     console.log('checking for winner... ... ...');
 
-    for(var piece in columns){
-      console.log(columns[piece]);
-    }
-
   },//end of checkForWinner method
 
-  'moves':0
+  'moves':0,
 
-}//end gameBoard Object
+
+  playConnectFour:function(){
+    var moves=this.moves;
+    var turnBlack=true;
+    console.log(moves,turnBlack);
+
+
+    while(this.moves<10){
+
+      if(turnBlack){
+        $('.col-1').click(function(){
+          player1.makeAMove('0');
+          break;
+        });
+        $('.col-2').click(function(){
+          player1.makeAMove('1');
+          break;
+        });
+        $('.col-3').click(function(){
+          player1.makeAMove('2');
+          break;
+        });
+        $('.col-4').click(function(){
+          player1.makeAMove('3');
+          break;
+        });
+        $('.col-5').click(function(){
+          player1.makeAMove('4');
+          break;
+        });
+        $('.col-6').click(function(){
+          player1.makeAMove('5');
+          break;
+        });
+        $('.col-7').click(function(){
+          player1.makeAMove('6');
+          break;
+        });
+      } else {
+        $('.col-1').click(function(){
+          player2.makeAMove('0');
+          break;
+        });
+        $('.col-2').click(function(){
+          player2.makeAMove('1');
+          break;
+        });
+        $('.col-3').click(function(){
+          player2.makeAMove('2');
+          break;
+        });
+        $('.col-4').click(function(){
+          player2.makeAMove('3');
+          break;
+        });
+        $('.col-5').click(function(){
+          player2.makeAMove('4');
+          break;
+        });
+        $('.col-6').click(function(){
+          player2.makeAMove('5');
+          break;
+        });
+        $('.col-7').click(function(){
+          player2.makeAMove('6');
+          break;
+        });
+      }//end of if-else
+
+      turnBlack=!turnBlack;
+      console.log('turn:',turnBlack);
+    }//end of while loop
+
+
+  }//end gameBoard Object
 
 function Player(color,board){
   this.color=color;
@@ -40,11 +106,11 @@ function Player(color,board){
    //max sure the board does not go past 6 rows high; let the player know if they cannot play a piece in that column
 
    //semantics with naming of columns
-   this.lastMoveColumn=column;
+   this.lastMoveColumn=column-1;
    if(gameBoard.columns[column].length<6){
 
      gameBoard.columns[column].push(color);
-     this.lastMoveRow=gameBoard.columns[column].length; //need to add one nameing conventions with rows
+     this.lastMoveRow=gameBoard.columns[column].length; //need to add one naming conventions with rows
      this.columnLabel=parseInt(column);
      console.log(this);
      this.renderPiece();
