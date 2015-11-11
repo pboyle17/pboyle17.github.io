eba31ffc951db9992764f71ba3c44b618c8fd389
Paul Boyle <pboyle17@gmail.com>
2015-11-11 08:16:33 -0600

    train work

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -9,6 +9,8 @@ $(document).ready(function(){
 
 var gameBoard = {
 
+  'lastMove':null,
+
   columns:[ [],[],[],[],[],[],[] ] ,
 
   printBoard:function(){
@@ -16,109 +18,139 @@ var gameBoard = {
   } , //end of printBoard method
 
   checkForWinner:function(){
-    console.log('checking for winner... ... ...');
+    var moves=this.moves;
+    var lastMove=this.lastMove;
+    console.log(moves);
+    console.log(lastMove);
 
+    this.checkDown(lastMove);
+    this.checkRight(lastMove);
+    this.checkLeft(lastMove);
+
+
+
+
+    if(moves>41){
+      console.log('its a tie!');
+    }
   },//end of checkForWinner method
+
+  checkDown:function(lastMove){
+    var testSquare=[lastMove[0],lastMove[1]-1];
+    console.log(lastMove,testSquare);
+    var colorLastMove=$('#row'+lastMove[1]+' > div.space.col-'+lastMove[0]).css('backgroundColor');
+    var colorTestSquare=$('#row'+testSquare[1]+' > div.space.col-'+testSquare[0]).css('backgroundColor');
+    console.log(colorLastMove,colorTestSquare);
+    console.log(colorLastMove==colorTestSquare);
+  },
+
+  checkRight:function(lastMove){
+    var testSquare=[lastMove[0]+1,lastMove[1]];
+    console.log(lastMove,testSquare);
+    var colorLastMove=$('#row'+lastMove[1]+' > div.space.col-'+lastMove[0]).css('backgroundColor');
+    var colorTestSquare=$('#row'+testSquare[1]+' > div.space.col-'+testSquare[0]).css('backgroundColor');
+    console.log(colorLastMove,colorTestSquare);
+    console.log(colorLastMove==colorTestSquare);
+  },
+
+  checkLeft:function(lastMove){
+    var testSquare=[lastMove[0]-1,lastMove[1]];
+    console.log(lastMove,testSquare);
+    var colorLastMove=$('#row'+lastMove[1]+' > div.space.col-'+lastMove[0]).css('backgroundColor');
+    var colorTestSquare=$('#row'+testSquare[1]+' > div.space.col-'+testSquare[0]).css('backgroundColor');
+    console.log(colorLastMove,colorTestSquare);
+    console.log(colorLastMove==colorTestSquare);
+  },
 
   'moves':0,
 
 
   playConnectFour:function(){
-    var moves=this.moves;
-    var turnBlack=true;
-    console.log(moves,turnBlack);
+    var turn=true;
 
-
-    while(this.moves<10){
-
-      if(turnBlack){
-        $('.col-1').click(function(){
+      $('.col-0').click(function(){
+        if(turn){
           player1.makeAMove('0');
-          break;
-        });
-        $('.col-2').click(function(){
-          player1.makeAMove('1');
-          break;
-        });
-        $('.col-3').click(function(){
-          player1.makeAMove('2');
-          break;
-        });
-        $('.col-4').click(function(){
-          player1.makeAMove('3');
-          break;
-        });
-        $('.col-5').click(function(){
-          player1.makeAMove('4');
-          break;
-        });
-        $('.col-6').click(function(){
-          player1.makeAMove('5');
-          break;
-        });
-        $('.col-7').click(function(){
-          player1.makeAMove('6');
-          break;
-        });
-      } else {
-        $('.col-1').click(function(){
+          turn=!turn;
+        } else {
           player2.makeAMove('0');
-          break;
-        });
-        $('.col-2').click(function(){
+          turn=!turn;
+        }
+      });
+      $('.col-1').click(function(){
+        if(turn){
+          player1.makeAMove('1');
+          turn=!turn;
+        } else {
           player2.makeAMove('1');
-          break;
-        });
-        $('.col-3').click(function(){
+          turn=!turn;
+        }
+      });
+      $('.col-2').click(function(){
+        if(turn){
+          player1.makeAMove('2');
+          turn=!turn;
+        } else {
           player2.makeAMove('2');
-          break;
-        });
-        $('.col-4').click(function(){
+          turn=!turn;
+        }
+      });
+      $('.col-3').click(function(){
+        if(turn){
+          player1.makeAMove('3');
+          turn=!turn;
+        } else {
           player2.makeAMove('3');
-          break;
-        });
-        $('.col-5').click(function(){
+          turn=!turn;
+        }
+      });
+      $('.col-4').click(function(){
+        if(turn){
+          player1.makeAMove('4');
+          turn=!turn;
+        } else {
           player2.makeAMove('4');
-          break;
-        });
-        $('.col-6').click(function(){
+          turn=!turn;
+        }
+      });
+      $('.col-5').click(function(){
+        if(turn){
+          player1.makeAMove('5');
+          turn=!turn;
+        } else {
           player2.makeAMove('5');
-          break;
-        });
-        $('.col-7').click(function(){
+          turn=!turn;
+        }
+      });
+      $('.col-6').click(function(){
+        if(turn){
+          player1.makeAMove('6');
+          turn=!turn;
+        } else {
           player2.makeAMove('6');
-          break;
-        });
-      }//end of if-else
-
-      turnBlack=!turnBlack;
-      console.log('turn:',turnBlack);
-    }//end of while loop
-
-
-  }//end gameBoard Object
+          turn=!turn;
+        }
+      });
+    }// end of playConnectFour method
+};//end gameBoard Object
 
 function Player(color,board){
   this.color=color;
-  this.lastMoveRow=1;
+  this.lastMoveRow=0;
   this.columnLabel=null;
  this.makeAMove=function(column){
    //max sure the board does not go past 6 rows high; let the player know if they cannot play a piece in that column
 
    //semantics with naming of columns
-   this.lastMoveColumn=column-1;
+   this.lastMoveColumn=column;
    if(gameBoard.columns[column].length<6){
 
      gameBoard.columns[column].push(color);
-     this.lastMoveRow=gameBoard.columns[column].length; //need to add one naming conventions with rows
+     this.lastMoveRow=gameBoard.columns[column].length-1;
      this.columnLabel=parseInt(column);
-     console.log(this);
      this.renderPiece();
      board.moves++;
      //once 7 moves have been played there is a possibility for a winner;check for it
-     if(board.moves>6){
-       board.checkForWinner();
-     }
    } else {
      //cannot play anymore pieces to this column; invalid move
      console.log('please choose another move; there is no move room in that column! :D');
@@ -129,10 +161,12 @@ function Player(color,board){
   this.renderPiece=function(){
     var col=this.columnLabel;
     var row=this.lastMoveRow.toString();
+    gameBoard.lastMove=[col,parseInt(row)];
 
-    console.log(color);
-    console.log('row',row,'col',col);
     $('#row'+row+'> div.space.col-'+col).css('background-color',color);
+    if(board.moves>5){
+      board.checkForWinner();
+    }
   };//end of renderPiece function
 
 };// end of Player constructor function
