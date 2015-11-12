dfa6c91af6570704ef74268baf5460621885a899
Paul Boyle <pboyle17@gmail.com>
2015-11-12 12:36:07 -0600

    commiting some updates

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -1,9 +1,13 @@
+
+
 $(document).ready(function(){
 
   gameBoard.playConnectFour();
 });//end of document.ready()
 var gameBoard = {
+  //each empty array represents a column in the connect for board.
   columns:[ [],[],[],[],[],[],[] ] ,
+  //each direction is an array length of 2 that when added to any piece coordinates will transform those coordinates to another space representing a change in that given direction.
   directions:{
     right:[1,0],
     left:[-1,0],
@@ -14,6 +18,7 @@ var gameBoard = {
     diagDownLeft:[-1,-1]
   },
 
+//an object that holds coordinates for a pieces that make a run of x number of pieces in a row.
   spacesInARow:{
     'first':[],
     'second':[],
@@ -21,6 +26,7 @@ var gameBoard = {
     'fourth':[],
   },
 
+//method that resets the spaces in a row object to empty arrays
   resetSpacesInARow:function(){
     gameBoard.spacesInARow.first=[];
     gameBoard.spacesInARow.second=[];
@@ -28,36 +34,48 @@ var gameBoard = {
     gameBoard.spacesInARow.fourth=[];
   },
 
-//coordinates of last move
+//set the coordinates of last move called in ln 427
   setLastMove:function(arr){
     gameBoard.lastMove=arr;
-    console.log('last move set');
+
+    // console.log('last move set');
   },
 
-//difference arr to transform lastMove coordinates to testSpace coordinates
+//  difference arr (directions ln 9) to transform lastMove coordinates to testSpace coordinates. add the columns of both arrays and the rows of both arrays
   setTestSpace:function(diff){
     gameBoard.testSpace=[gameBoard.lastMove[0]+diff[0],gameBoard.lastMove[1]+diff[1]]
-    console.log('test move set:',gameBoard.testSpace)
+    // console.log('test move set:',gameBoard.testSpace)
   },
 
-//find a color of some coordinates
+//find a color of some coordinates selecting with jQuery and usings $.css to get the value of background color and return that value;
   findColor:function(arr){
     var color=$('#row'+arr[1]+' > div.space.col-'+arr[0]).css('backgroundColor');
     return color;
   },
 
-//compare color of two different spaces
+//compare color of two different spaces;returns a BOOLEAN, uses method findColor(arr) ln:49 to grab the color of a set of coordinates
   compareColor:function(space1,space2){
-
     if(gameBoard.findColor(space1)==gameBoard.findColor(space2)){
-      console.log('the colors!!! they match!!!!');
-      console.log('============================');
+      // console.log('the colors!!! they match!!!!');
+      // console.log('============================');
       return true;
     } else {
       return false;
     }
 
   },
+
+  // render:function(){
+  //   if(gameBoard.turn){
+  //     player1.makeAMove('0');
+  //     gameBoard.turn=!gameBoard.turn;
+  //     $('#title').empty().append("Red's turn");
+  //   } else {
+  //     player2.makeAMove('0');
+  //     gameBoard.turn=!gameBoard.turn;
+  //     $('#title').empty().append("Black's Turn");
+  //   }
+  // },
 
   // incrementInARowCount:function(){
   //   gameBoard.inARowCount++;
@@ -69,23 +87,21 @@ var gameBoard = {
 
 //check to see if winning game state for either player;
   checkForWinner:function(direction){
-    console.log('checking to see if there is a winner');
-    console.log('=====================================')
-
-
+    // console.log('checking to see if there is a winner');
+    // console.log('=====================================')
       gameBoard.resetSpacesInARow();
       gameBoard.spacesInARow.first=gameBoard.lastMove;
-      console.log(gameBoard.spacesInARow.first);
+      // console.log(gameBoard.spacesInARow.first);
       if(gameBoard.checkMatch(gameBoard.directions.right)){
         gameBoard.spacesInARow.second=gameBoard.testSpace;
         gameBoard.lastMove=gameBoard.testSpace;
-        console.log(gameBoard.lastMove);
+        // console.log(gameBoard.lastMove);
         if(gameBoard.checkMatch(gameBoard.directions.right)){
           gameBoard.spacesInARow.third=gameBoard.testSpace;
           gameBoard.lastMove=gameBoard.testSpace;
           if(gameBoard.checkMatch(gameBoard.directions.right)){
             gameBoard.spacesInARow.fourth=gameBoard.testSpace;
-            console.log('winner winner winner!!!!');
+            // console.log('winner winner winner!!!!');
 
             for(var spaces in gameBoard.spacesInARow){
               var row = gameBoard.spacesInARow[spaces][1];
@@ -96,25 +112,25 @@ var gameBoard = {
             }
           }
         } else {
-          console.log('no winners yet')
+          // console.log('no winners yet')
         }
       } else {
-        console.log('nope no winners yet');
+        // console.log('nope no winners yet');
       }
 
       gameBoard.resetSpacesInARow();
       gameBoard.spacesInARow.first=gameBoard.lastMove;
-      console.log(gameBoard.spacesInARow.first);
+      // console.log(gameBoard.spacesInARow.first);
       if(gameBoard.checkMatch(gameBoard.directions.left)){
         gameBoard.spacesInARow.second=gameBoard.testSpace;
         gameBoard.lastMove=gameBoard.testSpace;
-        console.log(gameBoard.lastMove);
+        // console.log(gameBoard.lastMove);
         if(gameBoard.checkMatch(gameBoard.directions.left)){
           gameBoard.spacesInARow.third=gameBoard.testSpace;
           gameBoard.lastMove=gameBoard.testSpace;
           if(gameBoard.checkMatch(gameBoard.directions.left)){
             gameBoard.spacesInARow.fourth=gameBoard.testSpace;
-            console.log('winner winner winner!!!!');
+            // console.log('winner winner winner!!!!');
             for(var spaces in gameBoard.spacesInARow){
               var row = gameBoard.spacesInARow[spaces][1];
               var col = gameBoard.spacesInARow[spaces][0];
@@ -124,25 +140,25 @@ var gameBoard = {
             }
           }
         } else {
-          console.log('no winners yet')
+          // console.log('no winners yet')
         }
       } else {
-        console.log('nope no winners yet');
+        // console.log('nope no winners yet');
       }
 
       gameBoard.resetSpacesInARow();
       gameBoard.spacesInARow.first=gameBoard.lastMove;
-      console.log(gameBoard.spacesInARow.first);
+      // console.log(gameBoard.spacesInARow.first);
       if(gameBoard.checkMatch(gameBoard.directions.diagUpLeft)){
         gameBoard.spacesInARow.second=gameBoard.testSpace;
         gameBoard.lastMove=gameBoard.testSpace;
-        console.log(gameBoard.lastMove);
+        // console.log(gameBoard.lastMove);
         if(gameBoard.checkMatch(gameBoard.directions.diagUpLeft)){
           gameBoard.spacesInARow.third=gameBoard.testSpace;
           gameBoard.lastMove=gameBoard.testSpace;
           if(gameBoard.checkMatch(gameBoard.directions.diagUpLeft)){
             gameBoard.spacesInARow.fourth=gameBoard.testSpace;
-            console.log('winner winner winner!!!!');
+            // console.log('winner winner winner!!!!');
             for(var spaces in gameBoard.spacesInARow){
               var row = gameBoard.spacesInARow[spaces][1];
               var col = gameBoard.spacesInARow[spaces][0];
@@ -151,25 +167,25 @@ var gameBoard = {
             }
           }
         } else {
-          console.log('no winners yet')
+          // console.log('no winners yet')
         }
       } else {
-        console.log('nope no winners yet');
+        // console.log('nope no winners yet');
       }
 
       gameBoard.resetSpacesInARow();
       gameBoard.spacesInARow.first=gameBoard.lastMove;
-      console.log(gameBoard.spacesInARow.first);
+      // console.log(gameBoard.spacesInARow.first);
       if(gameBoard.checkMatch(gameBoard.directions.diagUpRight)){
         gameBoard.spacesInARow.second=gameBoard.testSpace;
         gameBoard.lastMove=gameBoard.testSpace;
-        console.log(gameBoard.lastMove);
+        // console.log(gameBoard.lastMove);
         if(gameBoard.checkMatch(gameBoard.directions.diagUpRight)){
           gameBoard.spacesInARow.third=gameBoard.testSpace;
           gameBoard.lastMove=gameBoard.testSpace;
           if(gameBoard.checkMatch(gameBoard.directions.diagUpRight)){
             gameBoard.spacesInARow.fourth=gameBoard.testSpace;
-            console.log('winner winner winner!!!!');
+            // console.log('winner winner winner!!!!');
             for(var spaces in gameBoard.spacesInARow){
               var row = gameBoard.spacesInARow[spaces][1];
               var col = gameBoard.spacesInARow[spaces][0];
@@ -178,24 +194,24 @@ var gameBoard = {
             }
           }
         } else {
-          console.log('no winners yet')
+          // console.log('no winners yet')
         }
       } else {
-        console.log('nope no winners yet');
+        // console.log('nope no winners yet');
       }
       gameBoard.resetSpacesInARow();
       gameBoard.spacesInARow.first=gameBoard.lastMove;
-      console.log(gameBoard.spacesInARow.first);
+      // console.log(gameBoard.spacesInARow.first);
       if(gameBoard.checkMatch(gameBoard.directions.diagDownRight)){
         gameBoard.spacesInARow.second=gameBoard.testSpace;
         gameBoard.lastMove=gameBoard.testSpace;
-        console.log(gameBoard.lastMove);
+        // console.log(gameBoard.lastMove);
         if(gameBoard.checkMatch(gameBoard.directions.diagDownRight)){
           gameBoard.spacesInARow.third=gameBoard.testSpace;
           gameBoard.lastMove=gameBoard.testSpace;
           if(gameBoard.checkMatch(gameBoard.directions.diagDownRight)){
             gameBoard.spacesInARow.fourth=gameBoard.testSpace;
-            console.log('winner winner winner!!!!');
+            // console.log('winner winner winner!!!!');
             for(var spaces in gameBoard.spacesInARow){
               var row = gameBoard.spacesInARow[spaces][1];
               var col = gameBoard.spacesInARow[spaces][0];
@@ -204,25 +220,25 @@ var gameBoard = {
             }
           }
         } else {
-          console.log('no winners yet')
+          // console.log('no winners yet')
         }
       } else {
-        console.log('nope no winners yet');
+        // console.log('nope no winners yet');
       }
 
       gameBoard.resetSpacesInARow();
       gameBoard.spacesInARow.first=gameBoard.lastMove;
-      console.log(gameBoard.spacesInARow.first);
+      // console.log(gameBoard.spacesInARow.first);
       if(gameBoard.checkMatch(gameBoard.directions.diagDownLeft)){
         gameBoard.spacesInARow.second=gameBoard.testSpace;
         gameBoard.lastMove=gameBoard.testSpace;
-        console.log(gameBoard.lastMove);
+        // console.log(gameBoard.lastMove);
         if(gameBoard.checkMatch(gameBoard.directions.diagDownLeft)){
           gameBoard.spacesInARow.third=gameBoard.testSpace;
           gameBoard.lastMove=gameBoard.testSpace;
           if(gameBoard.checkMatch(gameBoard.directions.diagDownLeft)){
             gameBoard.spacesInARow.fourth=gameBoard.testSpace;
-            console.log('winner winner winner!!!!');
+            // console.log('winner winner winner!!!!');
             for(var spaces in gameBoard.spacesInARow){
               var row = gameBoard.spacesInARow[spaces][1];
               var col = gameBoard.spacesInARow[spaces][0];
@@ -231,25 +247,26 @@ var gameBoard = {
             }
           }
         } else {
-          console.log('no winners yet')
+          // console.log('no winners yet')
         }
       } else {
-        console.log('nope no winners yet');
+        // console.log('nope no winners yet');
       }
 
       gameBoard.resetSpacesInARow();
       gameBoard.spacesInARow.first=gameBoard.lastMove;
-      console.log(gameBoard.spacesInARow.first);
+      // console.log(gameBoard.spacesInARow.first);
       if(gameBoard.checkMatch(gameBoard.directions.down)){
         gameBoard.spacesInARow.second=gameBoard.testSpace;
         gameBoard.lastMove=gameBoard.testSpace;
-        console.log(gameBoard.lastMove);
+        // console.log(gameBoard.lastMove);
         if(gameBoard.checkMatch(gameBoard.directions.down)){
           gameBoard.spacesInARow.third=gameBoard.testSpace;
           gameBoard.lastMove=gameBoard.testSpace;
           if(gameBoard.checkMatch(gameBoard.directions.down)){
             gameBoard.spacesInARow.fourth=gameBoard.testSpace;
-            console.log('winner winner winner!!!!');
+            // console.log('winner winner winner!!!!');
+//
             for(var spaces in gameBoard.spacesInARow){
               var row = gameBoard.spacesInARow[spaces][1];
               var col = gameBoard.spacesInARow[spaces][0];
@@ -258,10 +275,10 @@ var gameBoard = {
             }
           }
         } else {
-          console.log('no winners yet')
+          // console.log('no winners yet')
         }
       } else {
-        console.log('nope no winners yet');
+        // console.log('nope no winners yet');
       }
 
 
@@ -276,7 +293,7 @@ var gameBoard = {
     gameBoard.findColor(gameBoard.lastMove);
     gameBoard.findColor(gameBoard.testSpace);
     if(gameBoard.compareColor(gameBoard.lastMove,gameBoard.testSpace)){
-      console.log('direction',direction);
+      // console.log('direction',direction);
       return true;
     } else {
       return false;
@@ -284,80 +301,111 @@ var gameBoard = {
   },
 
   playConnectFour:function(){
-    var turn = gameBoard.turn
-    turn=true;
-    if(turn==true){
-      $('#title').empty().append('Black\'s Turn!');
-    }
-    if(turn==false){
-      $('#title').empty().append('Red\'s Turn!');
-    }
+    gameBoard.turn=true;
+    var turn = gameBoard.turn;
+
     gameBoard.moves=0;
     gameBoard.inARowCount=1;
 
+
+//Set event listener to every column if its blacks turn add a black piece , else add a red piece
       $('.col-0').click(function(){
         if(turn){
           player1.makeAMove('0');
           turn=!turn;
+          $('#title').empty().append("Red's Turn");
         } else {
           player2.makeAMove('0');
           turn=!turn;
+          $('#title').empty().append("Black's Turn");
+
         }
       });
       $('.col-1').click(function(){
         if(turn){
           player1.makeAMove('1');
           turn=!turn;
+          $('#title').empty().append("Red's Turn");
+
         } else {
           player2.makeAMove('1');
           turn=!turn;
+          $('#title').empty().append("Black's Turn");
+
         }
       });
       $('.col-2').click(function(){
         if(turn){
           player1.makeAMove('2');
           turn=!turn;
+          $('#title').empty().append("Red's Turn");
+
         } else {
           player2.makeAMove('2');
           turn=!turn;
+          $('#title').empty().append("Black's Turn");
+
         }
       });
       $('.col-3').click(function(){
         if(turn){
           player1.makeAMove('3');
           turn=!turn;
+          $('#title').empty().append("Red's Turn");
+
         } else {
           player2.makeAMove('3');
           turn=!turn;
+          $('#title').empty().append("Black's Turn");
+
         }
       });
       $('.col-4').click(function(){
         if(turn){
           player1.makeAMove('4');
           turn=!turn;
+          $('#title').empty().append("Red's Turn");
+
         } else {
           player2.makeAMove('4');
           turn=!turn;
+          $('#title').empty().append("Black's Turn");
+
         }
       });
       $('.col-5').click(function(){
         if(turn){
           player1.makeAMove('5');
           turn=!turn;
+          $('#title').empty().append("Red's Turn");
+
         } else {
           player2.makeAMove('5');
           turn=!turn;
+          $('#title').empty().append("Black's Turn");
+
         }
       });
       $('.col-6').click(function(){
         if(turn){
           player1.makeAMove('6');
           turn=!turn;
+          $('#title').empty().append("Red's Turn");
+
         } else {
           player2.makeAMove('6');
           turn=!turn;
+          $('#title').empty().append("Black's Turn");
+
         }
       });
+      // $('.col-1').click(gameBoard.render());
+      // $('.col-2').click(gameBoard.render());
+      // $('.col-3').click(gameBoard.render());
+      // $('.col-4').click(gameBoard.render());
+      // $('.col-5').click(gameBoard.render());
+      // $('.col-6').click(gameBoard.render());
+      // $('.col-0').click(gameBoard.render());
     }// end of playConnectFour method
 };//end gameBoard Object
 
@@ -374,12 +422,12 @@ function Player(color,board){
    //semantics with naming of columns
    this.lastMoveColumn=column;
    if(gameBoard.columns[column].length<6){
-
+     //if a valid move push color to the array representing the board
      gameBoard.columns[column].push(color);
      this.lastMoveRow=gameBoard.columns[column].length-1;
      this.columnLabel=parseInt(column);
      gameBoard.setLastMove([gameBoard.columns[column].length-1,parseInt(column)]);
-     this.renderPiece();
+     this.renderPiece();//ln 438
      gameBoard.moves++;
      //once 7 moves have been played there is a possibility for a winner;check for it
    } else {
@@ -403,7 +451,7 @@ function Player(color,board){
     var col=this.columnLabel;
     var row=this.lastMoveRow.toString();
     gameBoard.lastMove=[col,parseInt(row)];
-
+    //colors a particular square color depend which played played the move. color is an argument in the constructor function 463 464
     $('#row'+row+'> div.space.col-'+col).css('background-color',color);
     if(board.moves>5){
       board.checkForWinner();
@@ -411,8 +459,6 @@ function Player(color,board){
   };//end of renderPiece function
 
 };// end of Player constructor function
-
-
 
 var player1 = new Player ('black',gameBoard);
 var player2 = new Player ('red',gameBoard);
